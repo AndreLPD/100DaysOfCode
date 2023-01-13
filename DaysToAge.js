fc2ff672c5ec4d0a207942b7ddf526a5a6afc4c8
Andre Luis <andre.luis.pessoa@gmail.com>
2023-01-13 15:18:40 -0300

    Correção de output

--- DaysToAge.js
+++ DaysToAge.js
@@ -1,7 +1,11 @@
-const age = 31
+
+//Day 1/100 - Convert Age to Days
+
+const age = 31;
+
 function calcAge(age){
     if(typeof age !== "number") return "Is Only integers";
     return age*365;
 }
 
-calcAge(age);
+console.log(calcAge(age));
