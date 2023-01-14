5079a87ceba440a90529273703e55531b98bb582
AndreLPD <andre_luis_pessoa@hotmail.com>
2023-01-14 01:16:21 -0300

    Correção Find Nemo

--- FindNemo.js
+++ FindNemo.js
@@ -1,7 +1,7 @@
 const phrase = "I finding Nemo !";
 function findNemo(phrase){
-    const isNemo = phrase.split(" ").some(item => item === "Nemo");
-    return isNemo ? "Im found Nemo" : "I can't find Nemo";
+    const isNemo = phrase.split(" ").findIndex(item => item === "Nemo")+1;
+    return isNemo ? `Im found Nemo at ${isNemo}` : "I can't find Nemo";
 }
 
 console.log(findNemo(phrase));
