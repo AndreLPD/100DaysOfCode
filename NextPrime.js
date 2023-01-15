04df4d271900220a40b5025fc26e302cfccc155d
AndreLPD <andre_luis_pessoa@hotmail.com>
2023-01-15 00:02:50 -0300

    Initial Commit

---
+++ NextPrime.js
@@ -0,0 +1,27 @@
+const num = 24;
+
+function nextPrime(num){
+    if(num < 2) return num;
+        let bool = true;
+        while(bool){
+            if(calcPrime(num)){
+                bool = false;
+                return num;
+            }else{
+                num++;
+                calcPrime(num);
+            }
+        }
+}
+
+
+    function calcPrime(num){
+        for (let i = 2; i < num; i++) {
+            if(num % i === 0) {
+                return false;
+            }
+        }
+        return true
+    }
+
+console.log(nextPrime(num));
