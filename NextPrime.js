6373e3f308c99e0e3146a2837d50ca4942dc1972
AndreLPD <andre_luis_pessoa@hotmail.com>
2023-01-15 00:40:39 -0300

    Add Title

--- NextPrime.js
+++ NextPrime.js
@@ -1,4 +1,5 @@
-const num = 24;
+//Day 06 - Next Prime
+const num = 9;
 
 function nextPrime(num){
     if(num < 2) return num;
@@ -17,9 +18,7 @@ function nextPrime(num){
 
     function calcPrime(num){
         for (let i = 2; i < num; i++) {
-            if(num % i === 0) {
-                return false;
-            }
+            if(num % i === 0) return false;
         }
         return true
     }
